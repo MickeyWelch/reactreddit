cffcf779604bb19108cf48f645d2e47ace435efa
annemiekeSegersNovi <93577704+annemiekeSegersNovi@users.noreply.github.com>
2022-03-26 11:21:25 +0100

    opdracht 3 gemaakt, alles lijkt goed te werken

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import Home from "./pages/home/Home";
 import Subreddit from "./pages/subreddit/Subreddit";
 
 // sudo
-// note to self: netjes lezen en sudo schrijven per stap. Maak documenten schoon en doe basis opzet eerst! Maak hele opdracht af
+// note to self: netjes lezen en sudo schrijven per stap. Maak documenten schoon en doe basis opzet eerst! Maak hele opdracht af. per opdracht een commit
 // 1.1 doe is samen met opdrach 5
 // 2. Browserrouter in index.js, Switch en Route in app.js, useParam in Subreddit, navLinks in header
-// 3. useEffect, async funstion, get axios, log, paths in jsx zetten, map door posts
-
+// 3. install axsios, Home.js useEffect, async funstion, get axios, log, paths in jsx zetten, map door posts
 
 function App() {
   return (
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Link} from "react-router-dom";
 
 function Home() {
+    const [posts, getPosts] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const result = await axios.get('https://www.reddit.com/hot.json?limit=15');
+                console.log(result.data);
+                getPosts(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        fetchData();
+    }, [])
+
     return (
-        <p>Dit is Home</p>
+        <>
+            <h1>Hottest posts <span>on reddit right now</span></h1>
+            {posts && <>
+                    {posts.data.children.map((post) => {
+                        return <article>
+                            <h3><a href={post.data.url} target="_blank"
+                                   title={`go to ${post.data.title} on reddit`}>{post.data.title}</a></h3>
+                            <span className="meta">
+                            <p><Link to={`/subreddit/${post.data.subreddit_id}`}>{post.data.subreddit_name_prefixed}</Link></p>
+                            <p>Comments {post.data.num_comments} - Ups {post.data.ups}</p>
+                            </span>
+                        </article>
+                    })}
+            </>
+            }
+        </>
     );
 }
 
